d0e73ffb10bf0a3213f159cd9c6f895df1057e0b
Noman <pxycknomdictator@outlook.com>
2024-07-14 19:01:49 +0500

    design completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <div className="block text-center text-[1rem] sm:text-2xl font-[500] bg-secondaryLight dark:bg-secondaryDark py-6">
+      <div className="block text-center text-[1rem] sm:text-2xl font-[500] bg-secondaryLight dark:bg-secondaryDark py-6 text-secondaryDark dark:text-secondaryLight">
         © 2024 <span className="text-blue-500">Noman</span> All Rights Reserved.
       </div>
     </footer>
