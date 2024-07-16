44752ff7e81e69cb235af6357ed2ffd3891e72b7
Noman <pxycknomdictator@outlook.com>
2024-07-16 20:51:12 +0500

    optimize speed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -14,7 +14,7 @@ export default {
         secondaryLight: "hsl(240, 25%, 98%)",
       },
       fontFamily: {
-        logo: "Playwrite AU TAS, cursive",
+        logo: "logo",
       },
     },
   },
